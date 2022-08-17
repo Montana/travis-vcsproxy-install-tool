cb5206834f15ff943f2fd41738ece16ba77e1064
Montana Mendy <montana@linux.com>
2022-08-17 12:27:40 -0700

    push

---
+++ perforce.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Fetch P4 Helix Core CLI
+wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/perforce.list
+deb http://package.perforce.com/apt/ubuntu focal release
+EOF
+
+sudo apt-get update
+echo "yes" | sudo apt-get install helix-p4d
+
+# Create folders to test p4 build on 
+mkdir test-p4 
+
+# Go into the directory test-p4 and add a .travis.yml file to it (just a simple one), I'll have a simple one in this repo - so you can follow.
+
+# Create a ~/p4 folder, then ~/p4/p4config.txt file 
+mkdir ~/p4 
+mkdir ~/p4/p4config.txt 
+export P4CONFIG=~/p4/p4config.txt 
+
+# Create the p4config.txt file, and populate it with generic information that you must add your variables into
+echo "P4PORT=localhost:16666
+P4USER=testuser
+P4CHARSET=utf8
+P4CLIENT=testuserspace001" 
+>> ~/p4/p4config.txt
